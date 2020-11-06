14b5cb9d78401bc4ea328b973689f89d7f030851
Kushalkhemka <Kushalkhemka559@gmail.com>
2020-11-06 18:37:49 +0530

    dubbuged

--- sketch.js
+++ sketch.js
@@ -41,6 +41,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(200,50);
+    console.log(bird);
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
@@ -94,12 +95,14 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
+       bird.trajectory=[];
+       Matter.Body.setPosition(bird.body,{x:200,y:50});
        slingshot.attach(bird.body);
     }
 }
 
 async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var responseJSON = await response.json();
 
     var datetime = responseJSON.datetime;
